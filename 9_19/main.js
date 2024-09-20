79f5548fcd0a2fd327371984ff450bb3105e6a49
n0-p <kotetneko@icloud.com>
2024-09-20 04:50:18 +0000

    9月19日分を別ファイルに移動

---
+++ 9_19/main.js
@@ -0,0 +1,20 @@
+document.querySelector("h1").textContent = "自分の名前"
+document.querySelector("h2").textContent = "稲葉"
+
+
+
+//documentとは！！
+//htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。
+
+//DOM(ドム)
+//ドキュメントオブジェクトモデル（Document Object Model)の略で、webページの要素やコンテンツなどをツリー構造で表現したモデル
+
+//上記コードの意味
+//document.querySelector("h1").textContent = "自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//textContent = "自分の名前"
+
