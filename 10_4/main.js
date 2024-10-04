2000556f86fc271540c62b9b6a0eece81a5addf0
n0-p <kotetneko@icloud.com>
2024-10-04 06:14:45 +0000

    10_4 プルダウンリストの作成

---
+++ 10_4/main.js
@@ -0,0 +1,19 @@
+function errorinput(){
+    const month = document.getElementById("month").value;
+    const day = document.getElementById("day").value;
+    if(month<1 || month>12){
+        alert("適切な値ではありません")
+        return
+    }
+    console.log("month;",month,"day;",day)
+}
+
+const monthSelect = document.getElementById("month");
+monthSelectlist();
+function monthSelectlist(){
+    for(let i=1;i<12;i++)
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        monthSelect.appendChild(option);
+}
