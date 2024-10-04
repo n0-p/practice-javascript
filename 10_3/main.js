647fde6a93453ab39c9e870681b84f25f9eaf34d
n0-p <kotetneko@icloud.com>
2024-10-04 06:13:42 +0000

    10_4

--- 10_3/main.js
+++ 10_3/main.js
@@ -1,4 +1,9 @@
 function erorinput(){
     console.log(document.getElementById("month"))
     console.log(document.getElementById("day"))
+
+    const month = document.getElementById("month").value
+    const day = document.getElementById("day").value
+
+    console.log("month;",month,"day;",day)
 }
