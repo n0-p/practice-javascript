87014751c0d33a02e154ddbc9b8d928bcd581079
n0-p <kotetneko@icloud.com>
2025-02-13 05:20:49 +0000

    0213 js セレクタ

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+const gameSelector = document.getElementById('game-select');
+const select = document.createElement("select");
+    select.name = "games";
+    select.id = "game-select";
+    const options = [
+        { value: "", text: "--ゲームを選択してください--" },
+        { value: "clicker", text: "クリッカーゲーム" },
+        { value: "number-guess", text: "数当てゲーム" }
+    ];
+    options.forEach(optionsData => {
+        const option = document.createElement("option");
+        option.value = optionsData.value;
+        option.textContent = optionsData.text;
+        select.appendChild(option);
+    });
+    gameSelect.appendChild(select);
+
+
+
 function ClickCounterGame(){
     let count = 0;
-    const gameContainer = document.getElementById("game-container");
-
+    
     let button1 = document.createElement("button");
     button1.textContent = "+1ボタン";
 
@@ -28,13 +46,10 @@ function ClickCounterGame(){
         counter.textContent = count;
     })
 
-   
-
     button3.addEventListener("click",function(){
         count = 0;
         counter.textContent = count;
     })
-
 
     gameContainer.appendChild(button1);
     gameContainer.appendChild(button2);
@@ -44,6 +59,8 @@ function ClickCounterGame(){
 
 }
 ClickCounterGame();
+
+
 
 
 function startNumberGuessGame(){
